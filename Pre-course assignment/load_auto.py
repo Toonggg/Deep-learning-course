2a3d4c750a141d39fd0bcb14c2eb1172c9f8f8cb
Toonggg <48351012+Toonggg@users.noreply.github.com>
2021-03-18 13:29:05 +0100

    Added files.

---
+++ Pre-course assignment/load_auto.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+
+def load_auto():
+
+	# import data
+	Auto = pd.read_csv('/Users/tongyou/Desktop/Deep learning course/Auto.csv', na_values='?', dtype={'ID': str}).dropna().reset_index() 
+
+	# Extract relevant data features
+	X_train = Auto[['cylinders','displacement','horsepower','weight', 'acceleration','year','origin']].values
+	X_train_horsepower = Auto[['horsepower']].values
+	Y_train = Auto[['mpg']].values 
+
+	return X_train, X_train_horsepower, Y_train
