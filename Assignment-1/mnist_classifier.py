64453a1710123d7bbb16b27924dd44c95df35e39
Toonggg <48351012+Toonggg@users.noreply.github.com>
2021-03-31 10:54:09 +0200

    Added Lecture 3 slides and renamed Python script.

--- Assignment-1/mnist_classifier.py
+++ Assignment-1/mnist_classifier.py
@@ -2,4 +2,11 @@ import numpy as np
 from numba import jit 
 from load_mnist import load_mnist 
 
+
 xt, yt, xtt, ytt = load_mnist()
+
+M = 10 
+n = xt.shape[0] # 60000 
+p = xt.shape[1] # 784 
+
+print(xt.shape)
