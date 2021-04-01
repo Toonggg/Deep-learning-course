7ff53e5827e6c07aaf4bac431794049e8b484038
Toonggg <48351012+Toonggg@users.noreply.github.com>
2021-04-01 13:40:23 +0200

    Added first part of softmax implementation.

--- Assignment-1/mnist_classifier.py
+++ Assignment-1/mnist_classifier.py
@@ -1,12 +1,35 @@
 import numpy as np 
 from numba import jit 
 from load_mnist import load_mnist 
+from matplotlib import pyplot as plt 
 
+x_train, y_train, x_test, y_test = load_mnist()
 
-xt, yt, xtt, ytt = load_mnist()
+# shape x_train: 60000 x 784
+# shape y_train: 60000 x 10 
 
 M = 10 
-n = xt.shape[0] # 60000 
-p = xt.shape[1] # 784 
+n = x_train.shape[0] # number of training examples - 60000 
+p = x_train.shape[1] # number of input pixels - 784 (flattened 28x28 image) 
 
-print(xt.shape)
+def gradient_descent(xtrain, ytrain, lr, maxit): 
+
+    w_mj = np.random.normal(size = (M, p)) # weight matrix                                                                                                                                                                                                                        
+    b_m = np.zeros(shape = (1, M)) # offset vector 
+    z_im = np.zeros(shape = (n, M)) # model 
+
+    J = np.zeros(shape = (1, maxit)) # cost vector  
+
+    it = 0 
+
+    while it != maxit: 
+        for m in range(0, M):
+            for j in range(0, p):
+                z_im[:, m] = np.sum(w_mj[m, j] * xtrain[:, j]) + b_m[m]
+ 
+
+        it += 1
+
+    return J, it, w_mj, b_m , z_im
+
+J, it, wmj, bm, zim = gradient_descent(x_train, y_train, 0.01, 100) 
