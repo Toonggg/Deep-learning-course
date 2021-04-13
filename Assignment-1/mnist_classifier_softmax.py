Added cost and accuracy metrices for testing data, cleaned up code, and started with correct metric calculation for mini-batch gradient descent.

Assignment-1/mnist_classifier_softmax.py
@@ -4,12 +4,11 @@ from matplotlib import pyplot as plt
 
 x_train, y_train, x_test, y_test = load_mnist()
 
-# shape x_train: 60000 x 784
-# shape y_train: 60000 x 10 
-
 M = 10 
-n = x_train.shape[0] # number of training examples - 60000 
+n_train = x_train.shape[0] # number of training examples - 60000 
 p = x_train.shape[1] # number of input pixels - 784 (flattened 28x28 image) 
+
+n_test = x_test.shape[0]
 
 def predict_test(wmj, bm, xtest, ytest): 
 
@@ -27,18 +26,11 @@ def predict_test(wmj, bm, xtest, ytest):
 
     return z_predict, p_predict, acc_test 
 
-def init_arrays():
+def softmax_gd(xtrain, ytrain, xtest, ytest, n_train, n_test, lr, maxit): 
 
-
-
-    return None 
-
-def gradient_descent(xtrain, ytrain, xtest, ytest, lr, maxit): 
-
-    #w_mj = np.random.normal(size = (M, p)) # weight matrix 
     w_mj = np.random.rand(M, p) # weight matrix                                                                                                                                                                                                                                    
     b_m = np.zeros(shape = (1, M)) # offset vector 
-    z_im = np.zeros(shape = (n, M)) # model in (n x M) 
+    z_im = np.zeros(shape = (n_train, M)) # model in (n x M) 
     dJdbm = np.zeros(shape = (1, M))
     dJdwmj = np.zeros(shape = (M, p)) 
 
@@ -65,19 +57,18 @@ def gradient_descent(xtrain, ytrain, xtest, ytest, lr, maxit):
         dJdbm = np.sum(dJdzim, axis = 0) 
         dJdwmj = dJdzim.T @ xtrain 
                 
-        b_m = b_m - (1/n) * lr * dJdbm 
-        w_mj = w_mj - (1/n) * lr * dJdwmj
+        b_m = b_m - (1/n_train) * lr * dJdbm 
+        w_mj = w_mj - (1/n_train) * lr * dJdwmj
 
         # Cost and accuracy for training data
         L_i = np.sum(y_im * np.log(np.sum(np.exp(z_im_norm), axis = 1, keepdims = True)) - y_im * z_im_norm, axis = 1) 
-        J[it] = (1/n) * np.sum(L_i) 
+        J[it] = (1/n_train) * np.sum(L_i) 
 
         ypred_train = np.argmax(p_im, axis = 1) 
         ytrue_train = np.argmax(y_train, axis = 1) 
-        acc_train[it] = np.array([1 for i in range(0,n) if ypred_train[i] == ytrue_train[i]]).sum() 
+        acc_train[it] = np.array([1 for i in range(0,n_train) if ypred_train[i] == ytrue_train[i]]).sum() 
 
-        # Cost and accuracy for testing data
-        n_test = y_test.shape[0] 
+        # Cost and accuracy for testing data 
         z_test = xtest @ w_mj.T + b_m 
         z_test_norm = z_test - np.max(z_test, axis = 1, keepdims = True)
         L_i_test = np.sum(ytest * np.log(np.sum(np.exp(z_test_norm), axis = 1, keepdims = True)) - ytest * z_test_norm, axis = 1) 
@@ -90,24 +81,21 @@ def gradient_descent(xtrain, ytrain, xtest, ytest, lr, maxit):
         acc_test[it] = np.array([1 for i in range(0,n_test) if ypred_test[i] == ytrue_test[i]]).sum() 
                 
         it += 1 
-        print("Iteration: %s" % it) 
+        print("Iteration: (%s/%s)" % (it, maxit)) 
 
-    return J, acc_train * (1/n) * 100, J_test, acc_test * (1/n_test) * 100, it, w_mj, b_m , z_im, p_im 
+    return J, acc_train * (1/n_train) * 100, J_test, acc_test * (1/n_test) * 100, it, w_mj, b_m , z_im, p_im 
 
-J, acc_train, J_test, acc_test, it, wmj, bm, zim, pim = gradient_descent(x_train, y_train, x_test, y_test, 0.05, 1000) 
- 
-print("Total number of iterations: (%s/2000)" % it)  
+J, acc_train, J_test, acc_test, it, wmj, bm, zim, pim = softmax_gd(x_train, y_train, x_test, y_test, n_train, n_test, 0.02, 3000) 
 
-w_mj = wmj
-wmj /= np.max(wmj) # rescale weights by maximum for visualization 
+#wmj /= np.max(wmj) # rescale weights by maximum for visualization 
 
-### Cost, accuracy, and weights for training data 
 plt.figure(1) 
 plt_J, = plt.plot(J, 'r') 
 plt_J_test, = plt.plot(J_test, 'b') 
 plt.legend([plt_J, plt_J_test], ['Train cost', 'Test cost'])
 plt.xlabel('Number of iterations') 
 plt.ylabel('Cost J') 
+plt.title('Cost versus iterations') 
 print("Final train cost: %s" % float(J[-1]))
 print("Final test cost: %s" % float(J_test[-1]))
 
@@ -117,43 +105,30 @@ plt_acc_test, = plt.plot(acc_test, 'b')
 plt.legend([plt_acc_train, plt_acc_test], ['Train accuracy', 'Test accuracy']) 
 plt.xlabel('Number of iterations') 
 plt.ylabel('Accuracy in %%') 
+plt.title('Accuracy versus iterations') 
 print("Final train accuracy: %s%%" % float(acc_train[-1])) 
 print("Final test accuracy: %s%%" % float(acc_test[-1])) 
 
-figw, axw = plt.subplots(2, 5, figsize = (10,10)) 
-  
+figw, axw = plt.subplots(2, 5, figsize = (8,8)) 
 axw[0,0].imshow(wmj[0, :].reshape(28,28), cmap = 'gray') 
 axw[0,0].set_title('0') 
-
 axw[0,1].imshow(wmj[1, :].reshape(28,28), cmap = 'gray') 
 axw[0,1].set_title('1')
-
 axw[0,2].imshow(wmj[2, :].reshape(28,28), cmap = 'gray') 
 axw[0,2].set_title('2')
-
 axw[0,3].imshow(wmj[3, :].reshape(28,28), cmap = 'gray') 
 axw[0,3].set_title('3')
-
 axw[0,4].imshow(wmj[4, :].reshape(28,28), cmap = 'gray') 
 axw[0,4].set_title('4') 
-
 axw[1,0].imshow(wmj[5, :].reshape(28,28), cmap = 'gray') 
 axw[1,0].set_title('5')
-
 axw[1,1].imshow(wmj[6, :].reshape(28,28), cmap = 'gray') 
 axw[1,1].set_title('6')
-
 axw[1,2].imshow(wmj[7, :].reshape(28,28), cmap = 'gray') 
 axw[1,2].set_title('7')
-
 axw[1,3].imshow(wmj[8, :].reshape(28,28), cmap = 'gray') 
 axw[1,3].set_title('8')
-
 axw[1,4].imshow(wmj[9, :].reshape(28,28), cmap = 'gray') 
-axw[1,4].set_title('9')
-
-### Accuracy for test data 
-zp, pp, acc_test = predict_test(w_mj, bm, x_test, y_test)
-print("Test accuracy: %s%%" % acc_test) 
+axw[1,4].set_title('9') 
 
 plt.show()

Assignment-1/mnist_classifier_softmax_minibatch.py
@@ -5,94 +5,93 @@ from matplotlib import pyplot as plt
 x_train, y_train, x_test, y_test = load_mnist()
 
 M = 10 
-n = x_train.shape[0] # number of training examples - 60000 
+n_train = x_train.shape[0] # number of training examples - 60000 
 p = x_train.shape[1] # number of input pixels - 784 (flattened 28x28 image) 
+
+n_test = x_test.shape[0] # number of testing examples - 10000 
 
 n_batch = 64 # batch size 
 epochs = 1000 # epochs 
 
-def predict_test(): 
-    
-    return None
+def softmax_gd_minibatch(xtrain, ytrain, xtest, ytest, nb, lr_init, tau, n_train, n_test): 
 
-def gradient_descent(xtrain, ytrain): 
-
-    #w_mj = np.random.normal(size = (M, p)) # weight matrix 
-    w_mj = np.random.rand(M, p) # weight matrix - random initialization
+    w_mj = np.random.rand(M, p) 
                                                                                                                                                                                                                           
-    b_m = np.zeros(shape = (1, M)) # offset vector 
-    z_im = np.zeros(shape = (n, M)) # model in (n x M) 
+    b_m = np.zeros(shape = (1, M)) 
+    z_im = np.zeros(shape = (n_train, M)) 
     dJdbm = np.zeros(shape = (1, M)) 
     dJdwmj = np.zeros(shape = (M, p)) 
 
-    #J = np.zeros(shape = (maxit, 1)) # cost vector 
-    #c_acc = np.zeros(shape = (maxit, 1)) # classifcation accuracy 
-    J = []
-    c_acc = []
+    J_train = []
+    acc_train = [] 
 
-    lr0 = 0.3 
-    lrt = 0.01 * lr0 
-    tau = 1000 // 2 
+    J_test = []
+    acc_test = [] 
 
-    for e in range(0, epochs): 
+    e_p = 0 # epoch counter 
+    lr0 = lr_init # initial learning rate
+    lrt = 0.01 * lr0 # final learning rate 
+    t_tau = tau 
 
-        # Shuffling each epoch
+    while e_p != epochs: 
+
         ### Shuffling indices 
-        ind = np.arange(n)
+        ind = np.arange(n_train)
         np.random.shuffle(ind) # shuffle indices 
 
         ### Shuffling training data and labels
         xt = xtrain[ind, :] 
-
         yt = ytrain[ind, :] 
 
         ytrue_shuff = np.argmax(yt, axis = 1) 
 
         it = 0 
     
-        for j in range(n//n_batch): 
+        for j in range(n_train//nb): 
 
-                lr = (1 - (it/tau)) * lr0 + (it/tau) * lrt 
+            lr = (1 - (it/t_tau)) * lr0 + (it/t_tau) * lrt 
 
-                mini_batch = np.random.randint(0, n, size = n_batch) # batch indices 
+            mini_batch = np.random.randint(0, n_train, size = nb) # batch indices 
 
-                z_im[mini_batch, :] = xt[mini_batch, :] @ w_mj.T + b_m 
+            z_im[mini_batch, :] = xt[mini_batch, :] @ w_mj.T + b_m 
 
-                y_im = yt[mini_batch, :] 
+            y_im = yt[mini_batch, :] 
 
-                z_im_norm = z_im[mini_batch, :] - np.max(z_im[mini_batch, :], axis = 1, keepdims = True) 
+            z_im_norm = z_im[mini_batch, :] - np.max(z_im[mini_batch, :], axis = 1, keepdims = True) 
 
-                p_im = np.exp(z_im_norm) / np.sum(np.exp(z_im_norm), axis = 1, keepdims = True) 
+            p_im = np.exp(z_im_norm) / np.sum(np.exp(z_im_norm), axis = 1, keepdims = True) 
 
-                dJdzim = (y_im * p_im - y_im) 
-                dJdbm = np.sum(dJdzim, axis = 0) 
-                dJdwmj = dJdzim.T @ xt[mini_batch, :] 
+            dJdzim = (y_im * p_im - y_im) 
+            dJdbm = np.sum(dJdzim, axis = 0) 
+            dJdwmj = dJdzim.T @ xt[mini_batch, :] 
                 
-                b_m = b_m - (1/n_batch) * lr * dJdbm 
-                w_mj = w_mj - (1/n_batch) * lr * dJdwmj 
+            b_m = b_m - (1/nb) * lr * dJdbm 
+            w_mj = w_mj - (1/nb) * lr * dJdwmj 
 
-                ### TO-DO: fix loss/cost and accuracy calculations ### 
-                L_i = np.sum(y_im * np.log(np.sum(np.exp(z_im_norm), axis = 1, keepdims = True)) - y_im * z_im_norm, axis = 1) 
-                #J[it] = (1/n_batch) * np.sum(L_i) 
-                J.append((1/n_batch) * np.sum(L_i))
+            # Cost and accuracy for training data 
+            L_i = np.sum(y_im * np.log(np.sum(np.exp(z_im_norm), axis = 1, keepdims = True)) - y_im * z_im_norm, axis = 1) 
+            ypred = np.argmax(p_im, axis = 1) 
 
-                ypred = np.argmax(p_im, axis = 1) 
-                #c_acc[it] = np.array([1 for i in range(0,n_batch) if ypred[i] == ytrue_shuff[j]]).sum() 
-                c_acc.append((1/n_batch) * np.sum(ypred == ytrue_shuff[mini_batch]))
+            J_train.append((1/nb) * np.sum(L_i)) 
+            acc_train.append((1/nb) * np.sum(ypred == ytrue_shuff[mini_batch])) 
+
+            # Cost and accuracy for testing data 
                 
-                it += 1 
+            it += 1 
 
-    return J, c_acc, it, w_mj, b_m , z_im, p_im 
+        e_p += 1 
 
-J, c_acc, it, wmj, bm, zim, pim = gradient_descent(x_train, y_train) 
+    return J_train, acc_train, J_test, acc_test, it, w_mj, b_m , z_im, p_im 
 
-wmj /= np.max(wmj) 
+Jtrain, acc_train, Jtest, acc_test, it, wmj, bm, zim, pim = softmax_gd_minibatch(x_train, y_train, x_test, y_test, 0.3, 1000//2, n_batch, n_train, n_test) 
+
+#wmj /= np.max(wmj) 
 
 plt.figure(1)
-plt.plot(J) 
+plt.plot(Jtrain) 
 
 plt.figure(2) 
-plt.plot(c_acc) 
+plt.plot(acc_train) 
 
 plot_hist = False 
 if plot_hist == True: 