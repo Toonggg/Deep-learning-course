dc6daf00c6c3f28fb1f8eabd900ae36ccf77835e
Toonggg <48351012+Toonggg@users.noreply.github.com>
2021-04-17 20:54:47 +0200

    Started full nn implementation.

--- Assignment-1/mnist_classifier_full_nn.py
+++ Assignment-1/mnist_classifier_full_nn.py
@@ -4,19 +4,128 @@ from matplotlib import pyplot as plt
 
 x_train, y_train, x_test, y_test = load_mnist() 
 
-def relu_func():
+def relu(x):
+
+    return np.maximum(0, x)
+
+def sigmoid(x):
+
+    return (np.exp(x)) / (1 + np.exp(x))
+
+def relu_deriv(x):
+
+    if x > 0:
+        return x 
+
+def sigmoid_deriv(x):
+
+    return sigmoid(x) * (1-sigmoid(x))
+
+# We set these for now 
+L = 5 # number of layers - excluding input layer 
+n_hidden = np.array([500, 300, 150, 75]) # hidden units per layer - U_L 
+
+M = 10 
+p = x_train.shape[1] # number of input pixels - 784 (flattened 28x28 image) 
+
+n_train = x_train.shape[0] # number of training examples - 60000 
+n_test = x_test.shape[0] # number of testing examples - 10000 
+
+n_batch = 100 # batch size 
+epochs = 300 # number of epochs 
+
+ytrue_test = np.argmax(y_test, axis = 1) 
+
+def init_params():
+
+
+    for l in np.arange(0, L, 1):
+        W[l] = np.zeros()
+    w_mj = np.random.normal(scale = 0.01, size = (M, p)) # weight matrix                                                                                                                                                                                            
+    b_m = np.zeros(shape = (1, M)) 
+    z_im = np.zeros(shape = (n_train, M)) 
+
+    Q = np.zeros(shape = (n_batch, ))
+
+    dJdbm = np.zeros(shape = (1, M)) 
+    dJdwmj = np.zeros(shape = (M, p)) 
+    
 
     return None
 
-def sigmoid_func():
+def forward_prop():
+
+    for l in np.arange(0, L, 1):
+        h = 1
 
     return None
 
-def relu_deriv():
+def compute_cost():
+
+
+
+    return None 
+
+def backward_prop():
+
 
     return None
 
-def sigmoid_deriv():
+def update_parameters():
+
+
+    return None 
+
+def full_nn(xtrain, ytrain, xtest, ytest, ep, nb, lr_init, tau, n_train, n_test, k): 
+
+    e_p = 0 # epoch counter 
+    lr0 = lr_init # initial learning rate 
+    lrt = 0.01 * lr0 # final learning rate 
+    t_tau = tau 
+
+    tot_it = 0 
+
+    while e_p != ep: 
+
+        ### Shuffling indices 
+        ind = np.arange(n_train) 
+        np.random.shuffle(ind) # shuffle indices 
+
+        ### Shuffling training data and labels
+        xt = xtrain[ind, :] 
+        yt = ytrain[ind, :] 
+
+        ytrue_shuff = np.argmax(yt, axis = 1) 
+
+        it = 0 
+    
+        #for j in range(n_train//nb): 
+        while it != n_train//nb:
+
+            lr = (1 - (it/t_tau)) * lr0 + (it/t_tau) * lrt
+
+            mini_batch = np.random.randint(0, n_train, size = nb) # batch indices 
+
+            z_im[mini_batch, :] = xt[mini_batch, :] @ w_mj.T + b_m 
+
+            y_im = yt[mini_batch, :] 
+
+            z_im_norm = z_im[mini_batch, :] - np.max(z_im[mini_batch, :], axis = 1, keepdims = True) 
+
+            p_im = np.exp(z_im_norm) / np.sum(np.exp(z_im_norm), axis = 1, keepdims = True) 
+
+            dJdzim = (1/nb) * (y_im * p_im - y_im) 
+            dJdbm = np.sum(dJdzim, axis = 0) 
+            dJdwmj = dJdzim.T @ xt[mini_batch, :] 
+
+            b_m = b_m - lr * dJdbm 
+            w_mj = w_mj - lr * dJdwmj 
+
+            tot_it += 1
+            it += 1 
+
+            print("Epoch: (%s/%s), iteration: %s" % (e_p + 1, ep, it)) 
+
+        e_p += 1 
 
     return None
-
